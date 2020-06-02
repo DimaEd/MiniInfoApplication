0f0d15a36aba726db8d6b61ec95fc4988d9b29a1
DimaEd <ednach1987@mail.ru>
2020-06-02 17:48:54 +0300

    commit

--- src/main/java/com/ednach/config/WebConfiguration.java
+++ src/main/java/com/ednach/config/WebConfiguration.java
@@ -5,7 +5,10 @@ import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 public class WebConfiguration {
@@ -17,5 +20,14 @@ public class WebConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
     }
 }
